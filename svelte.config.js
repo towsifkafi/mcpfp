ddcd2381e3231a429a8471a27be1983d8bb7ac9c
Towsif Kafi <kafitowsif@gmail.com>
2023-09-06 00:23:48 +0600

    E

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
-//import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto';
 //import adapter from '@sveltejs/adapter-cloudflare';
-import adapter from '@sveltejs/adapter-netlify';
 import path from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
