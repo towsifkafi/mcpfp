725d1e9e0e7422c0c3b59c4827122171e726226c
Towsif12 <kafitowsif@gmail.com>
2023-09-06 17:27:14 +0000

    GG

--- src/lib/rendering/mojang.js
+++ src/lib/rendering/mojang.js
@@ -1,10 +1,12 @@
 async function getSkin(username) {
 	if (!valid(username)) return Promise.reject(`${username} is an invalid username`);
+	return `https://mineskin.eu/skin/${username}`
 	const UUID = await getUUID(username);
 	const response = await fetch(`https://sessionserver.mojang.com/session/minecraft/profile/${UUID}`);
 	if (!response.ok) return Promise.reject(`Response returned statuscode ${response.status}`);
 	const json = await response.json();
 	const r = JSON.parse(atob(json.properties[0].value));
+	console.log(r)
 	return r.textures.SKIN.url;
 }
 
