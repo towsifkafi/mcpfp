016f9f9ed972a3131f31580484e55d2c85e19d04
kafitowsif <kafitowsif@gmail.com>
2024-10-03 17:52:03 +0000

    fix: build process

--- api/src/index.ts
+++ api/src/index.ts
@@ -13,7 +13,7 @@ const app = new Hono()
 
 app.use(logger())
 
-app.use('/*', serveStatic({ root: './static' }))
+app.use('/*', serveStatic({ root: './web' }))
 
 app.get('/api/pfp/:overlay/:file{.+\\.(png|jpg|webp)$}', async (c) => {
 
--- api/static/_app/immutable/entry/start.ClyFgcDv.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.9ec5ljKw.js";export{t as start};
